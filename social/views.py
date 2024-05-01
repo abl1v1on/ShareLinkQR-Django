ab812c3a775cfb805d6242f15d4049e11e33b403
abl1v <abl1v1on@yandex.ru>
2024-05-01 15:08:03 +0500

    deleted database queries

--- social/views.py
+++ social/views.py
@@ -1,15 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-import qrcode
 
 from .forms import AddSocialNetworkForm
-from .models import SocialNetwork
+from .utils import QRCode, social_utils
 
 
 @login_required
 def index(request):
-    social_networks = SocialNetwork.objects.filter(user_id=request.user.id)
+    social_networks = social_utils.get_social_networks_by_filter(user_id=request.user.id)
 
     context = {
         'title': 'Мои социальные сети',
@@ -24,9 +23,9 @@ def add_social_network(request):
         form = AddSocialNetworkForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            if SocialNetwork.objects.filter(user_id=request.user.id, name=cd['name']).exists():
-                SocialNetwork.objects.filter(user_id=request.user.id, name=cd['name']).delete()
-            SocialNetwork.objects.create(name=cd['name'], link=cd['link'], user_id=request.user.id)
+            if social_utils.get_social_networks_by_filter(user_id=request.user.id, name=cd['name']).exists():
+                social_utils.get_social_networks_by_filter(user_id=request.user.id, name=cd['name']).delete()
+            social_utils.create_social_network(name=cd['name'], link=cd['link'], user_id=request.user.id)
             return redirect('social:home')
     else:
         form = AddSocialNetworkForm()
@@ -35,22 +34,19 @@ def add_social_network(request):
 
 @login_required
 def qr_code_view(request):
-    url = f'http://127.0.0.1:8000/social-networks/{request.user.id}/'
-    filename = f'static/qrCodes/{request.user.username}_qrcode.png'
-    img = qrcode.make(url)
-    img.save(filename)
+    qr = QRCode()
+    qr.generate_qrcode(request)
 
     context = {
         'title': 'Мой QR код',
-        'path_to_qrcode': filename,
+        'path_to_qrcode': qr.get_filename(),
     }
     return render(request, 'user-qr-code.html', context)
 
 
-@login_required
 def social_networks_view(request, user_id):
-    if SocialNetwork.objects.filter(user_id=user_id).exists():
-        social_networks = SocialNetwork.objects.filter(user_id=user_id)
+    if social_utils.get_social_networks_by_filter(user_id=user_id).exists():
+        social_networks = social_utils.get_social_networks_by_filter(user_id=user_id)
     else:
         return redirect('social:home')
 
@@ -59,3 +55,5 @@ def social_networks_view(request, user_id):
         'social_networks': social_networks
     }
     return render(request, 'social-network-list.html', context)
+
+# Удалил запросы к бд
