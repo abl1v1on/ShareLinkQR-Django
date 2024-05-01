49fcfea52aa956edd49a609722d8d43c344c7812
abl1v <abl1v1on@yandex.ru>
2024-05-01 15:07:43 +0500

    create SocialNetworkDetail endpoint and deleted database queries

--- api/views.py
+++ api/views.py
@@ -9,6 +9,7 @@ from rest_framework import generics
 
 from .serializers import MyTokenObtainPairSerializer, SocialNetworkSerializer, RegisterSerializer
 from social.models import SocialNetwork
+from . import utils
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -25,7 +26,7 @@ class RegisterView(generics.CreateAPIView):
 @permission_classes([IsAuthenticated])
 def get_social_networks(request):
     if request.method == 'GET':
-        social_networks = SocialNetwork.objects.filter(user_id=request.user.id)
+        social_networks = utils.get_social_network_by_filter(user_id=request.user.id)
         serializer = SocialNetworkSerializer(social_networks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
@@ -35,3 +36,17 @@ def get_social_networks(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class SocialNetworkDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = SocialNetworkSerializer
+    queryset = SocialNetwork.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        if utils.is_author(self.get_object(), request.user.id):
+            return super().put(request, *args, **kwargs)
+        return Response({'message': 'Ошибка доступа'})
+    
+
+# Создал endpoint SocialNetworkDetail и удалил запросы к бд
